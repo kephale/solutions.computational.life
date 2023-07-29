5977509ed1a4b5b52e9f23c43d7406bbf0c28c07
Kyle Harrington <czi@kyleharrington.com>
2023-07-29 12:31:50 -0400

    Update Home

--- gatsby/src/gatsby-theme-album/components/home.js
+++ gatsby/src/gatsby-theme-album/components/home.js
@@ -2,7 +2,7 @@ import React from "react"
 
 const Home = () => {
   return (
-    <p>Welcome!</p>
+      <p>Welcome to solutions.computational.life! Go to <a href="https://solutions.computational.life/about">for more information about this catalog</a> and <a href="https://solutions.computational.life/catalog">for the catalog listing</a>.</p>
   )
 }
 
