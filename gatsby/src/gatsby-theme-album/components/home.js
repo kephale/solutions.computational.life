0ea97fcf34c2c5b69d06684614482c3f290c04de
Kyle Harrington <czi@kyleharrington.com>
2023-08-01 08:19:24 -0400

    Single line

--- gatsby/src/gatsby-theme-album/components/home.js
+++ gatsby/src/gatsby-theme-album/components/home.js
@@ -1,9 +1,8 @@
 import React from "react"
 
 const Home = () => {
-  return (`<p>Welcome to solutions.computational.life! Check out these links <a href="https://solutions.computational.life/about">for more information about this catalog</a> and <a href="https://solutions.computational.life/catalog">for the catalog listing</a>.</p>
-      <p>The catalog is mainted by <a href="https://kyleharrington.com">Kyle Harrington</a>.
-`
+    return (
+        <p>Welcome to solutions.computational.life! Check out these links <a href="https://solutions.computational.life/about">for more information about this catalog</a> and <a href="https://solutions.computational.life/catalog">for the catalog listing</a>.</p><p>The catalog is mainted by <a href="https://kyleharrington.com">Kyle Harrington</a>.
   )
 }
 
