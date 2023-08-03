c34b9fba70212c63ddb6de5f7d5d160110dd2a20
Kyle Harrington <czi@kyleharrington.com>
2023-08-03 09:13:13 -0400

    Remove background colors

--- gatsby/src/GlobalStyle.js
+++ gatsby/src/GlobalStyle.js
@@ -3,14 +3,6 @@
 import { createGlobalStyle } from 'styled-components';
 
 const GlobalStyle = createGlobalStyle`
-  header {
-    background-color: ${({ theme }) => theme.colors.background};
-    /* You can also add other global styles here */
-  }
-  body {
-    background-color: ${({ theme }) => theme.colors.background};
-    /* You can also add other global styles here */
-  }
 `;
 
 export default GlobalStyle;
--- gatsby/src/gatsby-theme-album/components/home.js
+++ gatsby/src/gatsby-theme-album/components/home.js
@@ -12,11 +12,6 @@ export const wrapRootElement = ({ element }) => (
 );
 
 const Home = () => {
-  const containerStyle = {
-    backgroundColor: "rgb(75, 9, 85)",
-    /* Add other styles as needed */
-  };
-
   return (
       <p>Welcome to solutions.computational.life! Go to <a href="https://solutions.computational.life/about">for more information about this catalog</a> and <a href="https://solutions.computational.life/catalog">for the catalog listing</a>.</p>
   )
