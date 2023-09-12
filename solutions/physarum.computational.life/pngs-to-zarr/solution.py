000206696f373a1d7f7580cd465b19000f15c3e1
Kyle Harrington <czi@kyleharrington.com>
2023-09-12 12:07:04 -0400

    Adding new/updated physarum.computational.life_pngs-to-zarr_0.0.5

--- solutions/physarum.computational.life/pngs-to-zarr/solution.py
+++ solutions/physarum.computational.life/pngs-to-zarr/solution.py
@@ -5,62 +5,51 @@ from album.runner.api import setup
 # Please import additional modules at the beginning of your method declarations.
 # More information: https://docs.album.solutions/en/latest/solution-development/
 
-env_file = StringIO(
-    """channels:
-  - conda-forge
-  - defaults
-dependencies:
-  - python=3.10
-  - zarr
-  - numpy
-  - imageio
-"""
-)
-
-import os
-import subprocess
-import time
-from datetime import datetime
-import imageio
-import concurrent.futures
-
-def process_image(idx_image_file_tuple, directory, z):
-    idx, image_file = idx_image_file_tuple
-    if idx % 10 == 0:
-        print(f"Processed {idx} of {len(image_files)}")
-    img = imageio.imread(os.path.join(directory, image_file))
-    z[idx, ...] = img
 
 def read_images_to_zarr(directory, zarr_path):
     """
     Read RGB PNG images from a directory and store them in a Zarr array.
-    
+
     Args:
         directory (str): Path to the directory containing the images.
         zarr_path (str): Path to the Zarr array where images will be stored.
     """
-    import zarr    
-    
+    import os
+    import imageio.v2 as imageio
+    import numpy as np
+    import zarr
+
+    from ome_zarr.io import parse_url
+    from ome_zarr.writer import write_image
+
     # Get the list of image files in the directory
     image_files = [f for f in os.listdir(directory) if f.endswith('.png')]
     image_files.sort()  # sort filenames to maintain order
-    
+
     # Load the first image to get shape and data type
     sample_img = imageio.imread(os.path.join(directory, image_files[0]))
     img_shape = sample_img.shape
     img_dtype = sample_img.dtype
 
-    # Initialize the NestedDirectoryStore and Zarr array
-    store = zarr.NestedDirectoryStore(zarr_path)
-    z = zarr.open_array(store, mode='w', shape=(len(image_files),) + img_shape, dtype=img_dtype)
+    stack = np.zeros(
+        tuple([len(image_files)] + list(img_shape)), dtype=img_dtype
+    )
 
-    # Parallelize
-    with concurrent.futures.ThreadPoolExecutor() as executor:
-        executor.map(process_image, 
-                     [(idx, image_file) for idx, image_file in enumerate(image_files)],
-                     [directory] * len(image_files),
-                     [z] * len(image_files))
-    
+    for idx, image_file in enumerate(image_files):
+        stack[idx, :, :, :] = imageio.imread(
+            os.path.join(directory, image_file)
+        )
+
+    os.mkdir(zarr_path)
+    store = parse_url(zarr_path, mode="w").store
+    root = zarr.group(store=store)
+    write_image(
+        image=stack,
+        group=root,
+        axes="tczyx",
+        storage_options=dict(chunks=img_shape),
+    )
+
     print(f"All images stored in {zarr_path}")
 
 
@@ -73,7 +62,7 @@ def run():
 setup(
     group="physarum.computational.life",
     name="pngs-to-zarr",
-    version="0.0.4",
+    version="0.0.5",
     title="Convert PNGs to zarr.",
     description="An Album solution for converting a directory of PNGs into a zarr",
     solution_creators=["Kyle Harrington"],
@@ -97,5 +86,11 @@ setup(
         },
     ],
     run=run,
-    dependencies={"environment_file": env_file},
+    dependencies={
+        "parent": {
+            "group": "physarum.computational.life",
+            "name": "parent-environment",
+            "version": "0.0.2",
+        }
+    },
 )
