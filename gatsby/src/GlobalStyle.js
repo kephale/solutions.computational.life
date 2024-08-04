be68c48ea98d3b1c9e27b4073852f40743d2f9ab
Kyle Harrington <czi@kyleharrington.com>
2024-08-04 10:48:16 -0400

    Make it work more like the other catalogs

--- gatsby/src/GlobalStyle.js
+++
@@ -1,8 +0,0 @@
-// src/GlobalStyle.js
-
-import { createGlobalStyle } from 'styled-components';
-
-const GlobalStyle = createGlobalStyle`
-`;
-
-export default GlobalStyle;
--- gatsby/src/theme.js
+++
@@ -1,9 +0,0 @@
-// src/theme.js
-
-const theme = {
-  colors: {
-    background: '#750955', // Hex value: 75, 9, 85
-  },
-};
-
-export default theme;
